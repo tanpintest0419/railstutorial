7082f7d3d2fd9eda2045f6873e606afd8e4b3b8e
tanpintest <tanpintest0419@gmail.com>
2025-09-12 18:01:25 +0900

    full_title helper 追加、4章終了

--- src/chapter3/app/helpers/application_helper.rb
+++ src/chapter3/app/helpers/application_helper.rb
@@ -1,2 +1,11 @@
 module ApplicationHelper
+  # ページごとの完全なタイトルを返します。
+  def full_title(page_title = '')
+    base_title = "Ruby on Rails Tutorial Sample App"
+    if page_title.empty?
+      base_title                        # 暗黙の戻り値
+    else
+      page_title + " | " + base_title   # 文字列の結合
+    end
+  end
 end
