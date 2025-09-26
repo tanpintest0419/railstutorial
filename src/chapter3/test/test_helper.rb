4894b715d0b42083df6f603212a815faccfd3a1d
tanpintest <tanpintest0419@gmail.com>
2025-09-26 15:18:05 +0900

    chapter9終了

--- src/chapter3/test/test_helper.rb
+++ src/chapter3/test/test_helper.rb
@@ -18,5 +18,23 @@ module ActiveSupport
       !session[:user_id].nil?
     end
 
+    # テストユーザーとしてログインする
+    def log_in_as(user)
+      session[:user_id] = user.id
+    end
+
+  end
+end
+
+class ActionDispatch::IntegrationTest
+  # 統合テスト用: 実際にログインリクエストを投げてログイン
+  def log_in_as(user, password: "password", remember_me: "1")
+    post login_path, params: {
+      session: {
+        email: user.email,
+        password: password,
+        remember_me: remember_me
+      }
+    }
   end
 end
