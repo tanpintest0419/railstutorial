8965445c11509741b36d143a4013b56be9ac1187
tanpintest <tanpintest0419@gmail.com>
2025-10-09 17:26:52 +0900

    13.3.4まで

--- src/chapter3/app/controllers/users_controller.rb
+++ src/chapter3/app/controllers/users_controller.rb
@@ -65,14 +65,15 @@ class UsersController < ApplicationController
       @user = User.find(params[:id])
     end
 
-    # ログイン済みユーザーかどうか確認
-    def logged_in_user
-      unless logged_in?
-        store_location
-        flash[:danger] = "Please log in."
-        redirect_to login_url
-      end
-    end
+    # application_controller.rbに移動
+    # # ログイン済みユーザーかどうか確認
+    # def logged_in_user
+    #   unless logged_in?
+    #     store_location
+    #     flash[:danger] = "Please log in."
+    #     redirect_to login_url
+    #   end
+    # end
 
     # 正しいユーザーかどうか確認
     def correct_user
